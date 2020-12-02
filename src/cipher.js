82f995e9839cab8ec6e978ca3fb148d5bf241b33
valevilchis <vilchisvaleria11@gmail.com>
2020-12-02 12:56:04 -0600

    Pruebas encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
 const cipher = {
-  // ...
+  encode() {
+    let textoEntrada = "Hola me llamo Valeria";
+    /*Conteo de carcater por caracter*/
+    for (var i=0; i<=textoEntrada.length; i++) {
+      let nuevoTexto = textoEntrada.charCodeAt(i);
+      console.log(nuevoTexto);
+    }
+  }, 
+
+  decode(desplazamiento, entradaTexto) {
+
+  }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher.decode);
