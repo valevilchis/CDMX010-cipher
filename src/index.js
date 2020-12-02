86c185909054cbc89b3ea8bfced9f5b9e1971b2f
valevilchis <vilchisvaleria11@gmail.com>
2020-12-02 13:26:51 -0600

    Lectura de caracter y observar valor ASCII

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
-console.log(cipher.decode);
+function encode() {
+    let textoEntrada = "ABC";
+    /*Conteo de carcater por caracter*/
+    for (var i=0; i<=textoEntrada.length; i++) {
+      let caracterSalida = textoEntrada.charCodeAt(i);
+      console.log(caracterSalida);
+    }
+}
+
+console.log(encode);
+window.encode = encode;
