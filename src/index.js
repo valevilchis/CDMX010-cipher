813aa79128d18a612d36650e61851d16de48fbbe
valevilchis <vilchisvaleria11@gmail.com>
2020-12-02 23:49:19 -0600

    Cifrado de letras ABC con formula ascii

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import cipher from './cipher.js';
 
 function encode() {
     let textoEntrada = "ABC";
+    let desplazamiento = 8;
     /*Conteo de carcater por caracter*/
     for (var i=0; i<=textoEntrada.length; i++) {
       let caracterSalida = textoEntrada.charCodeAt(i);
       console.log(caracterSalida);
+
+      let caracterDespl = ((caracterSalida - 65 + desplazamiento) % 26 + 65);
+      let cifrado = String.fromCharCode(caracterDespl);
+      console.log(cifrado);
+     
     }
 }
 
