75364430105e44a7980b708cf93ec543fcecdcf2
valevilchis <vilchisvaleria11@gmail.com>
2020-12-10 01:34:21 -0600

    detalles en encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,10 @@
 const cipher = {
-  /*encode() {
-    let textoEntrada = "Hola me llamo Valeria";
-    Conteo de carcater por caracter
-    /*for (var i=0; i<=textoEntrada.length; i++) {
-      let nuevoTexto = textoEntrada.charCodeAt(i);
-      console.log(nuevoTexto);*
-    }
-  }, 
-  decode(desplazamiento, entradaTexto) {
-  }
-*/
       encode (textoEntrada,desplazamiento) {
         let textoSalida = "";
         for (let i=0; i < textoEntrada.length; i++) {
           let caracterSalida = textoEntrada.charCodeAt(i);
           let caracterDespla = ((caracterSalida - 65 + desplazamiento) % 26 + 65);
-          textoSalida += String.fromCharCode(caracterDespla);
+          textoSalida = textoSalida + String.fromCharCode(caracterDespla);
         }
         return textoSalida;
       },
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 
 import cipher from './cipher.js';
-/*function encode(password, desplazamiento) {
-    let textoEntrada = password;
-    let desplazamiento = desplazamiento;
-    /*Conteo de carcater por caracter
-    for (var i=0; i<=textoEntrada.length; i++) {
-      let caracterSalida = textoEntrada.charCodeAt(i);
-      console.log(caracterSalida);
-
-      let caracterDespl = ((caracterSalida - 65 + desplazamiento) % 26 + 65);
-      let cifrado = String.fromCharCode(caracterDespl);
-      console.log(cifrado);
-    }
-}
-function recibir() {
-    var valor = document.getElementById("texto").value;
-    document.getElementById("usr").innerHTML=valor;
-    var valor = document.getElementById("password").value;
-    document.getElementById("decode").innerHTML=valor; 
-} */
 
 let userEntrada = "";
 let passEntrada = "";
 let offset = 0;
-let passSalida = "";
 
-function encode() {
+document.getElementById('encodeM').addEventListener("click", encode, false);
+
+function encode(){
     userEntrada = document.getElementById("user").value;
     passEntrada = (document.getElementById("password").value).toUpperCase();
     offset = Number.parseInt(document.getElementById("desplazamiento").value);
-
+    let passSalida = "";
+    let descifrar = document.getElementById("decode");
+    
     if (userEntrada == "") {
         alert("Usuario requerido.");
     } else if (passEntrada == "") {
@@ -38,15 +22,29 @@ function encode() {
         alert("Seleccione un rango de desplazamiento.")
     } else {
         document.getElementById("usr").innerHTML = userEntrada;
-        /*document.getElementById("decode").innerHTML = passEntrada;*/
         passSalida = cipher.encode(passEntrada,offset);
         document.getElementById("encode").innerHTML = passSalida;
+        descifrar.style.display='block';
     }
-}
+};
+
+document.getElementById('decodeM').addEventListener("click", decode, false);
 
 function decode (){
+    userEntrada = document.getElementById("user").value;
+    passEntrada = (document.getElementById("codigo").value).toUpperCase();
+    let ofssetUser = Number.parseInt(document.getElementById("desplazamiento").value);
+    let passDescifrada = "";
 
-
+    if (passEntrada == "") {
+        alert("Codigo requerido");
+    } else {
+        document.getElementById("usr-d").innerHTML = userEntrada;
+        document.getElementById("encode-d").innerHTML = passEntrada;
+        document.getElementById("off-d").innerHTML = ofssetUser;
+        passDescifrada = cipher.decode(passEntrada,ofssetUser);
+        document.getElementById("decode-d").innerHTML = passDescifrada;
+    }
 }
 
 window.encode = encode;
